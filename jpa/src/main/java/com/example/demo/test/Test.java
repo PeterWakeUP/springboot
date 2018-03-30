2d3d41eb429945a6fc4558050c2e55eee3757642
苏文辉 <787297031@>
2018-03-30 10:26:14 +0800

    add something

--- jpa/src/main/java/com/example/demo/test/Test.java
+++ jpa/src/main/java/com/example/demo/test/Test.java
@@ -1,7 +1,16 @@
 package com.example.demo.test;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by 苏文辉 on 2018/3/30.
  */
+@RestController
 public class Test {
+
+    @RequestMapping("Test/say")
+    public String say(){
+        return "Hello";
+    }
 }
