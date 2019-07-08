72ba4dde2f7ef2ed478ea4ac3322ab402e94f3d1
Peter_shw <787297031@qq.com>
2019-07-08 23:33:53 +0800

    swagger

--- mybatis/src/main/java/com/example/demo/controller/ManController.java
+++ mybatis/src/main/java/com/example/demo/controller/ManController.java
@@ -2,31 +2,50 @@ package com.example.demo.controller;
 
 import com.example.demo.dao.ManMapper;
 import com.example.demo.domain.Man;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by 苏文辉 on 2018/4/2.
  */
 @RestController
+@Api(description = "Man信息管理")
 public class ManController {
 
     @Autowired
     ManMapper manMapper;
 
-    @RequestMapping("test")
-    public String test(){
+    //@ApiIgnore
+    @ApiOperation(value="测试", notes="test")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "query"),
+            @ApiImplicitParam(name = "name", value = "用户NAME", required = true, dataType = "String", paramType = "query")
+    })
+    @ApiResponses({
+            @ApiResponse(code=200,message="请求成功"),
+            @ApiResponse(code=400,message="请求参数没填好"),
+            @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对")
+    })
+    @PostMapping("test")
+    public String test(String id, String name){
         return "ManController";
     }
 
-    @RequestMapping("man/getManById/{id}")
-    public String getManById(@PathVariable(value = "id") int id){
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "path")
+    @ApiResponses({
+            @ApiResponse(code=200,message="请求成功"),
+            @ApiResponse(code=400,message="请求参数没填好"),
+            @ApiResponse(code=404,message="请求路径没有或页面跳转路径不对")
+    })
+    @RequestMapping(value = "man/getManById/{id}", method = RequestMethod.GET)
+    public Man getManById(@PathVariable(value = "id") int id){
         Man man = manMapper.selectById(id);
-        String str = "id:"+man.getId()+ ",name:"+ man.getName();
-        return str;
+        return man;
     }
 
 }
--- mybatis/src/main/java/com/example/demo/dao/ManMapper.java
+++ mybatis/src/main/java/com/example/demo/dao/ManMapper.java
@@ -8,10 +8,10 @@ import org.apache.ibatis.annotations.Select;
 /**
  * Created by 苏文辉 on 2018/4/2.
  */
-@Mapper //这个注解是每个mapper都要用，而@MapperScan("com.example.demo.dao")是把包里面的都注入了
+//@Mapper //这个注解是每个mapper都要用，而@MapperScan("com.example.demo.dao")是把包里面的都注入了
 public interface ManMapper {
 
-    @Select("select * from man where id = #{id}")
+    //@Select("select * from man where id = #{id}")
     //没找到selectById的实现，没找到ManMapper.xml文件, 注解可以
     Man selectById(@Param("id") Integer id);
 }
