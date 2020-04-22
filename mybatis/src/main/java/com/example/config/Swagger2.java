ac8a66b4b471969ab7b50626d26b2f76118fc442
Peter_shw <787297031@qq.com>
2020-04-22 14:14:43 +0800

    swagger

--- mybatis/src/main/java/com/example/config/Swagger2.java
+++ mybatis/src/main/java/com/example/config/Swagger2.java
@@ -23,7 +23,7 @@ public class Swagger2 {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("cn.saytime.web"))
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- mybatis/src/main/java/com/example/demo/domain/Man.java
+++ mybatis/src/main/java/com/example/demo/domain/Man.java
@@ -1,11 +1,20 @@
 package com.example.demo.domain;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiModelProperty;
+import springfox.documentation.annotations.ApiIgnore;
+
 /**
  * Created by 苏文辉 on 2018/4/2.
  */
+@Api(description = "man")
 public class Man {
 
+    @ApiModelProperty(value = "id", name = "id", required = true, example = "1")
     private Integer id;
+
+    @ApiModelProperty(hidden = true)
     private String name;
 
     public Integer getId() {
